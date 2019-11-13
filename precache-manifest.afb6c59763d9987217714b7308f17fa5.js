6e18c9583ac48d91d32ef0763730b4d402e8db8f
Shigi-p <shigi.522@gmail.com>
2019-11-13 20:35:00 +0900

    Updates

---
+++ precache-manifest.afb6c59763d9987217714b7308f17fa5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a867206cb38d8ec267cdd8b6e9173202",
+    "url": "/shigi_portfolio/index.html"
+  },
+  {
+    "revision": "a26bf600f0e41bdbcd4d",
+    "url": "/shigi_portfolio/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "bc410f8bebf2c7e45fb4",
+    "url": "/shigi_portfolio/static/js/2.e8af8b7a.chunk.js"
+  },
+  {
+    "revision": "a26bf600f0e41bdbcd4d",
+    "url": "/shigi_portfolio/static/js/main.ae13ff26.chunk.js"
+  },
+  {
+    "revision": "2ceae1147380be77b222",
+    "url": "/shigi_portfolio/static/js/runtime-main.394cb943.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/shigi_portfolio/static/media/logo.25bf045c.svg"
+  }
+]);
