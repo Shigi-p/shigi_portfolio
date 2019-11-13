15cd3b533bb31924b26c585b66dc6d5fe43c68c5
Shigi-p <shigi.522@gmail.com>
2019-11-13 21:45:05 +0900

    change UI

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,26 +1,72 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state ={
+        page: 0,
+    };
+    this.pages = this.pages.bind(this);
+    this.bookmark = this.bookmark.bind(this);
+    this.showBookmark = this.showBookmark.bind(this);
+    this.goNextpage = this.goNextpage.bind(this);
+    this.goPreviouspage = this.goPreviouspage.bind(this);
+  }
+
+  pages(page){
+    switch(page){
+      case 0:
+        return(
+          <div className={`Page${this.state.page}`}></div>
+        );
+      case 1:
+        return(
+          <div className={`Page${this.state.page + 1}`}>
+            <h1>This is my portfolio.</h1>
+          </div>
+        );
+    }
+  }
+
+  bookmark(){
+    return(
+      <div className="Bookmark" onClick={this.showBookmark()}>
+      </div>
+    );
+  }
+
+  showBookmark(){
+
+  }
+
+  goNextpage(){
+    this.setState({
+      page: this.state.page + 1,
+    });
+  }
+
+  goPreviouspage(){
+    this.setState({
+      page: this.state.page - 1,
+    });
+  }
+
+  render(){
+    return (
+      <div className="App">
+        {this.bookmark()}
+        <div className="LeftPage">
+          <span className="NextpageButton" onClick={() => this.goNextpage()} />
+          {this.pages(this.state.page + 1)}
+        </div>
+        <div className="RightPage">
+          {this.pages(this.state.page)}
+          <span className="PreviouspageButton" onClick={() => this.goPreviouspage()} />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
